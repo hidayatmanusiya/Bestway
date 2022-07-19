2adc99dd69ff284f5dded74db20e5f590b22f92b
Hidayat ali <107534037+hidayatbizmap@users.noreply.github.com>
2022-07-19 20:31:07 +0530

    delete file

--- bestway/bestway/doctype/employee_pay/employee_pay.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022, Bestway and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Employee Pay', {
-	// refresh: function(frm) {
-
-	// }
-});
--- bestway/bestway/doctype/employee_pay/test_employee_pay.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, Bestway and Contributors
-# See license.txt
-
-# import frappe
-import unittest
-
-class TestEmployeePay(unittest.TestCase):
-	pass
